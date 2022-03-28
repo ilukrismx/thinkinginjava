3874f09e68ae80ca1a22c3fb470240060cb46fe0
Ricardo Lopez <ricardo_adrianlopezcruz@comcast.com>
2022-03-28 18:06:22 -0400

    Guaranteed initialization with the constructor

---
+++ src/main/java/initialization/SimpleConstructor.java
@@ -0,0 +1,17 @@
+package initialization;
+
+//Demonstration of a simple constructor
+
+class Rock {
+    Rock() { //This is the constructor
+        System.out.println("Rock ");
+    }
+}
+
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock();
+        }
+    }
+}
---
+++ src/main/java/initialization/SimpleConstructor2.java
@@ -0,0 +1,19 @@
+package initialization;
+
+//Constructors can have arguments
+
+class Rock2 {
+
+    Rock2(int i) {
+        System.out.println("Rock " + i);
+    }
+}
+
+public class SimpleConstructor2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock2(i);
+        }
+    }
+}
